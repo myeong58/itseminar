15630cd5e8ab507dfdd4f90009721159fec35141
qhdud214 <38932405+qhdud214@users.noreply.github.com>
2018-12-03 20:15:00 +0900

    Update home.php

--- food/home.php
+++ food/home.php
@@ -5,7 +5,6 @@ header("content-type:text/html; charset=utf-8");
 include_once ("lib/common.php");
 include_once ("lib/dbconn.php");
 include_once ("head.php");
-include_once ("search.php");
 ?>
 <?php
  //if ($_GET['mode'] == "reply")   if ($_GET['mode'] == "modify")
